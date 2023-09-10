35e8aa11c8571e637805621d5738536a7be44129
chinedu-daniel <danielpreciouschinedu@gmail.com>
2023-09-09 23:39:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char k;
+
+for (k = 'a'; k <= 'z'; k++)
+putchar(k);
+putchar('\n');
+
+return (0);
+
+}
