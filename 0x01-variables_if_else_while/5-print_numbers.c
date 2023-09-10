2a1f5be8b474700513ca91ada9e03246d18070c9
chinedu-daniel <danielpreciouschinedu@gmail.com>
2023-09-10 01:38:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description - prints number base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10, a++);
+
+	putchar('\n');
+
+	return (0);
+}
