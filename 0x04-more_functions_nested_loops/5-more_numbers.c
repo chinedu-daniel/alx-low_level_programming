0ee2dd5842ef124e5715643a65f827881ebca47f
chinedu-daniel <danielpreciouschinedu@gmail.com>
2023-09-14 18:34:29 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,14 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
