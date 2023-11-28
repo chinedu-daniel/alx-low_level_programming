7f171c3dfcc1f62116e33fa0c907a0c72167a7c6
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2023-11-28 02:49:03 -0800

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - main function
+ * @name: function 1
+ * @age: functio 2
+ * @owner: function 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
