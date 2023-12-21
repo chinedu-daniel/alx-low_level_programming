05dc33151114abcf31038f61afd7d71c34bcd082
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2023-12-21 07:41:11 -0800

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,29 +17,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 	ptr = malloc(sizeof(char) * letters);
-	if (ptr == NULL)
-	{
-		close(fd);
+	if (!ptr)
 		return (0);
-	}
 	nr = read(fd, ptr, letters);
-	if (nr == -1)
+	if (nr < 0)
 	{
 		free(ptr);
-		close(fd);
 		return (0);
 	}
+	ptr[nr] = '\0';
+
+	close(fd);
+
 	nw = write(STDOUT_FILENO, ptr, nr);
-	if (nw == -1 || (size_t)nw != (size_t)nr)
+	if (nw < 0)
 	{
 		free(ptr);
-		close(fd);
 		return (0);
 	}
-	return (nr);
-	close(nw);
 	free(ptr);
+	return (nw);
 }
