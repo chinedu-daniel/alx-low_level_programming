9a64d309e3f64fafebd39ad534c4a81941c3fc46
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-16 04:19:38 -0800

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to delete.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	for (unsigned long int i = 0; i < ht->size; ++i)
+	{
+		hash_node_t *temp = ht->array[i];
+
+		while (temp != NULL)
+		{
+			hash_node_t *next = temp->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
