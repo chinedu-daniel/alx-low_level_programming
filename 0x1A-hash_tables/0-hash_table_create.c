718c0052d930fd9ef7ca8bdc090d75bbefbf01db
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-16 00:20:19 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a new hash table with the specified size.
+ *
+ * @size: The size of the array.
+ *
+ * Return: hash table on success, or NULL on failure.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return (NULL);
+
+	return (hash_table);
+}
+
