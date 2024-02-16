64f16146ec0b4dbcfd52c1873e3386464e3fbfea
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-16 02:24:17 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,12 +16,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table = malloc(sizeof(hash_table_t));
-
 	if (hash_table == NULL)
 		return (NULL);
 
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
